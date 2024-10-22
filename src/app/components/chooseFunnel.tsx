ae2c943d2063b7cb77d989420a9bf4351e91de71
itsmiraz <mirajulislamofficial044@gmail.com>
2024-10-22 22:50:45 +0600

    new changes

--- src/app/components/chooseFunnel.tsx
+++ src/app/components/chooseFunnel.tsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
 
 const ChooseFunnel = () => {
-  return (
-    <div>ChooseFunnel</div>
-  )
-}
+  const planData = [
+    {
+      title: "Free Plan",
+      desc: "Dip your toes in with",
+      credit: "150 credits",
+      active: false,
+    },
+    {
+      title: "Basic Plan",
+      desc: "",
+      credit: "800 credits/month",
+      bottomDesc: "to kickstart your funnel empire",
+      active: false,
+    },
+    {
+      title: "Pro Plan",
+      desc: "",
+      credit: "3500 credits/month",
+      bottomDesc: "for serious funnel builders",
+      active: true,
+    },
+    {
+      title: "Enterprise Plan",
+      desc: "",
+      credit: "Unlimited credits",
+      active: true,
+    },
+    {
+      title: "Free Plan",
+      desc: "Dip your toes in with",
+      credit: "150",
+      active: false,
+    },
+  ];
 
-export default ChooseFunnel
+  return (
+    <div className="pb-[140px]">
+      <h1 className="text-3xl font-bold text-center text-[#F7F9FB]">
+        Choose your path to Funnel success
+      </h1>
+      {/* <div className="mt-[50px]">
+        {planData.map((item, i) => (
+          <div
+            className="bg-[#001B2E99] py-[30px] flex justify-center items-center tailored-solutions-card-shadow"
+            key={i}
+          >
+
+ 
+
+          </div>
+        ))}
+      </div> */}
+    </div>
+  );
+};
+
+export default ChooseFunnel;
