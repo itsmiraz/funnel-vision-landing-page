f542e8ec1272c170d084377cb755490e6104fabd
itsmiraz <mirajulislamofficial044@gmail.com>
2024-10-23 21:48:55 +0600

    new animation added

--- src/app/components/chooseFunnel.tsx
+++ src/app/components/chooseFunnel.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import BlueCheck from "@/assets/icons/BlueCheck.svg";
 import LeftArrowIcon from "@/assets/icons/SmallLeftArrow.svg";
 import OrangeFillLeftArrow from "@/assets/icons/OrangeFillLeftArrow.svg";
-
+import Image from "next/image";
+import GradientBubble from "@/assets/images/smallBubble.png";
 const ChooseFunnel = () => {
   const planData = [
     {
@@ -44,64 +45,81 @@ const ChooseFunnel = () => {
       <div className="mt-[50px]  flex justify-center items-center gap-[25px]">
         {planData.map((item, i) => (
           <div
-            className="bg-[#001B2E99] pl-[30px] pr-[20px] w-[246px] py-[30px] flex flex-col justify-center items-center rounded-[20px] custom-card-inner-shadow  "
             key={i}
+            className={`${
+              item.active &&
+              "p-[10px] overflow-hidden  relative  rounded-[20px]"
+            }`}
           >
-            <div className="">
-              <h2 className="text-[20px] text-center font-medium font-roboto leading-[30px]">
-                {item.title}
-              </h2>
-              {item.desc && (
-                <p className="text-[12px] font-opensans font-semibold">
-                  {item.desc}
-                </p>
-              )}
+            <div className="bg-[#001B2E99] backdrop-blur-md relative z-30 pl-[30px] pr-[20px] w-[246px] py-[30px] flex flex-col justify-center items-center rounded-[20px] custom-card-inner-shadow  ">
+              <div className="">
+                <h2 className="text-[20px] text-center font-medium font-roboto leading-[30px]">
+                  {item.title}
+                </h2>
+                {item.desc && (
+                  <p className="text-[12px] font-opensans font-semibold">
+                    {item.desc}
+                  </p>
+                )}
+              </div>
+
+              {/* credits */}
+              <div className="pt-[23px] text-center">
+                <div
+                  className="text-[14px] font-medium font-roboto  text-[F7F9FB]"
+                  dangerouslySetInnerHTML={{ __html: item.credit }}
+                ></div>
+                {item.bottomDesc && (
+                  <p className="text-[12px] font-semibold font-opensans leading-[18px]">
+                    {item.bottomDesc}
+                  </p>
+                )}
+              </div>
+
+              <ul className="pt-[25px] space-y-[10px]">
+                <li className="flex items-center gap-x-3">
+                  <div>
+                    <BlueCheck />{" "}
+                  </div>{" "}
+                  <p> Lorem Ipsum is simply </p>
+                </li>
+                <li className="flex items-center gap-x-3">
+                  <div>
+                    <BlueCheck />{" "}
+                  </div>{" "}
+                  <p> Lorem Ipsum is simply </p>
+                </li>
+                <li className="flex items-center gap-x-3">
+                  <div>
+                    <BlueCheck />{" "}
+                  </div>{" "}
+                  <p> Lorem Ipsum is simply </p>
+                </li>
+                <li className="flex items-center gap-x-3">
+                  <div>
+                    <BlueCheck />{" "}
+                  </div>{" "}
+                  <p> Lorem Ipsum is simply </p>
+                </li>
+              </ul>
+
+              <button
+                className={`${
+                  item.active ? "bg-[#FF6B35]" : "bg-[#001B2E99] text-[#FF6B35]"
+                } text-[14px] border border-[#F7F9FB]/10 flex mt-[34px] font-mont font-bold w-fit mx-auto pl-3 justify-center gap-x-[11px] items-center p-1 rounded-[10px] `}
+              >
+                <p> Get Started</p>
+                {item.active ? <LeftArrowIcon /> : <OrangeFillLeftArrow />}
+              </button>
             </div>
 
-            {/* credits */}
-            <div className="pt-[23px] text-center">
-              <div
-                className="text-[14px] font-medium font-roboto  text-[F7F9FB]"
-                dangerouslySetInnerHTML={{ __html: item.credit }}
-              ></div>
-              {item.bottomDesc && (
-                <p className="text-[12px] font-semibold font-opensans leading-[18px]">
-                  {item.bottomDesc}
-                </p>
-              )}
-            </div>
-
-            <ul className="pt-[25px] space-y-[10px]">
-              <li className="flex items-center gap-x-3">
-                <div>
-                  <BlueCheck />{" "}
-                </div>{" "}
-                <p> Lorem Ipsum is simply </p>
-              </li>
-              <li className="flex items-center gap-x-3">
-                <div>
-                  <BlueCheck />{" "}
-                </div>{" "}
-                <p> Lorem Ipsum is simply </p>
-              </li>
-              <li className="flex items-center gap-x-3">
-                <div>
-                  <BlueCheck />{" "}
-                </div>{" "}
-                <p> Lorem Ipsum is simply </p>
-              </li>
-              <li className="flex items-center gap-x-3">
-                <div>
-                  <BlueCheck />{" "}
-                </div>{" "}
-                <p> Lorem Ipsum is simply </p>
-              </li>
-            </ul>
-
-            <button className={`${item.active ? 'bg-[#FF6B35]':"bg-[#001B2E99] text-[#FF6B35]" } text-[14px] border border-[#F7F9FB]/10 flex mt-[34px] font-mont font-bold w-fit mx-auto pl-3 justify-center gap-x-[11px] items-center p-1 rounded-[10px] `}>
-              <p> Get Started</p>
-              {item.active ? <LeftArrowIcon /> : <OrangeFillLeftArrow />}
-            </button>
+            {item.active && (
+              <Image
+                className="w-[307px] animate-funnel-card-gradient-animation absolute -top-10 -left-10 "
+                src={GradientBubble}
+                alt=""
+              />
+            )}
           </div>
         ))}
       </div>
